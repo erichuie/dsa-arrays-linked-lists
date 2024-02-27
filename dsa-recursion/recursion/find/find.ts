ebb8890a0b536f21dbd0d8a0264f0581847b0ab7
Max Shpungin <maxsconsulting@gmail.com>
2024-02-27 14:25:38 -0800

    completed find

--- dsa-recursion/recursion/find/find.ts
+++ dsa-recursion/recursion/find/find.ts
@@ -1,7 +1,17 @@
 /** find: return t/f is val is in arr. */
 
 function find(arr: number[], val: number): boolean {
-  return true;
+  if(arr[0] === val) return true
+  if(arr.length === 0) return false
+
+  return find(arr.slice(1), val);
 }
 
 export { find };
+
+
+
+// const numbers = [1, 2, 3];
+
+// find(animals, 1);  // true
+// find(animals, 0);   // false
