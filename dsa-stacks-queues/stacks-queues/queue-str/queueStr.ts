4da43edf31c22791134314de4cb5a972cb9756ac
Max Shpungin <maxsconsulting@gmail.com>
2024-02-27 13:04:24 -0800

    completed queue

--- dsa-stacks-queues/stacks-queues/queue-str/queueStr.ts
+++ dsa-stacks-queues/stacks-queues/queue-str/queueStr.ts
@@ -1,28 +1,35 @@
+import { LL } from '../common/ll';
+
 /** QueueStr: can remove from start or add to end */
 
 class QueueStr {
+  queueLL: LL<string>;
 
   constructor(initial: string[] = []) {
+    this.queueLL = new LL(initial);
   }
 
   /** enqueue(val): add val to end. Returns undefined. */
   enqueue(val: string): void {
+    this.queueLL.push(val);
   }
 
   /** dequeue(): remove & return item from start.
    * Throws error if empty. */
   dequeue(): string {
-    return "x";
+    if (this.queueLL.length === 0) throw new Error();
+    return this.queueLL.shift();
   }
 
   /** peek(): return the value of top. */
   peek(): string {
-    return "x";
+    if (this.queueLL.length === 0) throw new Error();
+    return this.queueLL.getAt(0);
   }
 
   /** peek(): return the value of top. */
   isEmpty(): boolean {
-    return false;
+    return this.queueLL.length === 0;
   }
 }
 
