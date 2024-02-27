73647af96f76a3279a78da58368c2d0e251140a9
Max Shpungin <maxsconsulting@gmail.com>
2024-02-27 14:08:11 -0800

    completed longest

--- dsa-recursion/recursion/longest/longest.ts
+++ dsa-recursion/recursion/longest/longest.ts
@@ -3,11 +3,12 @@ import { LLNodeStr } from "../common/ll";
 /** longest: return length of longest word in a linked list of words. */
 
 function longest(words: LLNodeStr | null): number {
-  // let currLongest = currLongest > words.val.length ? currLongest : words.val.length;
-  if(words.next === null) return words.val;
-  return longest(words.next)
+  if(words === null) return 0;
+  if(words!.next === null) return words!.val.length;
 
-  if(currLongest < words.val.length) currLongest = words.val.length;
+  let longestlength = longest(words!.next)
+
+  return words!.val.length > longestlength ? words!.val.length : longestlength;
 }
 
 export { longest };
